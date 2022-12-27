48c568c8a37a3bc4370d2d279530b10cd9f25217
Matias Molina Bishop <molibi01@gmail.com>
2022-12-27 13:45:29 +0100

    add link

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -4,9 +4,8 @@ function Header() {
     return (
         <div>
             <div class='links-style'>
-                <Link className="link-color-about" to='/project'>LINK HERE!!!!</Link>
-                <a className="link-color-about" href="/mysite/#/project">Projects</a>
-                <a className="link-color-about" href="/mysite/">About</a>
+                <Link className="link-color-about" to='/project'>Projects</Link>
+                <Link className="link-color-about" to='/'>About</Link>
                 <a className="link-color-about" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1nw_oA2NyTq69CwmaHIMQSzdyYYQvgkOl/view?usp=share_link">Resume</a>
             </div>
             <div className="top-container">
